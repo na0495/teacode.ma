3da1c62b56af24aa65b359e3199f58714c35a45a
Driss Boumlik <boumlik.t@gmail.com>
2021-02-06 00:15:53 +0100

    Add brand logo to footer

--- resources/views/partials/index/footer.blade.php
+++ resources/views/partials/index/footer.blade.php
@@ -3,7 +3,13 @@
         <div class="container w-100 py-3">
             <div class="row">    <!-- row-eq-height"> -->
                 <div class="col-md-6 col-sm-6 copyright-txt">
-                    <p>Copyright <i class="fas fa-copyright"></i> {{ now()->year }} | <a href="/">TeaCode.ma</a></p>
+                    <div>Copyright <i class="fas fa-copyright"></i> {{ now()->year }} |
+                        <div class="d-inline-block">
+                            <img src="{{ asset('/assets/img/teacode/teacode.png') }}"
+                            class="img-fluid rounded-circle square-30" alt="">
+                            <a href="/">TeaCode.ma</a>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-md-6 col-sm-6 owner">
                     <p>Made with <i class="fas fa-heart"></i> by <span class="bold">Driss Boumlik</span></p>
