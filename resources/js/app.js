e1c8e2aa6897552cab45adc47116b7ccdd439653
Driss Boumlik <boumlik.t@gmail.com>
2021-02-10 06:01:28 +0100

    Add loading animation on ready

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,4 +4,7 @@ require('./bootstrap');
 $(function () {
 
     particlesJS.load('particles-js', '/plugins/particles/particles.json');
+    setTimeout(() => {
+        $('.loader-wrapper').addClass('disappear');
+    }, 1000);
 });
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&amp;display=swap" rel="stylesheet">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('/plugins/bootstrap/css/bootstrap.min.css') }}">
         <link href="{{ asset('/assets/fontawesome/css/all.css') }}" rel="stylesheet">
         <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
@@ -15,13 +16,9 @@
         <script src="{{ asset('/plugins/particles/particles.js') }}"></script>
         <script src="{{ asset('/js/app.js') }}"></script>
 
-        <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/TweenMax-latest-beta.js"></script>
-        <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/MorphSVGPlugin.min.js"></script>
+        {{-- <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/TweenMax-latest-beta.js"></script> --}}
+        {{-- <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/MorphSVGPlugin.min.js"></script> --}}
         <title>@yield('title')</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
     </head>
     <body class="antialiased">
         <div class="wrapper">
@@ -32,6 +29,11 @@
                 {{-- <div class="resume">
                     <a href="https://resume.teacode.ma" target="_blank">my resume <i class="far fa-hand-point-right"></i></a>
                 </div> --}}
+        </div>
+        <div class="loader-wrapper">
+            <div class="loader">
+                <img src="{{ asset('/assets/img/teacode/teacode.png') }}" alt="">
+            </div>
         </div>
     </body>
 </html>
