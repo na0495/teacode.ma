5bd5277c759899bd3e94f06002cdc5e98667974b
Driss Boumlik <boumlik.t@gmail.com>
2021-02-19 19:16:51 +0100

    Edit social links section

--- resources/views/partials/index/find_us.blade.php
+++ resources/views/partials/index/find_us.blade.php
@@ -1,15 +1,15 @@
-<div id="find_us" class="find_us section-wrapper py-50px">
+<div id="find_us" class="find_us section-wrapper py-50px tc-white-bg">
     <div class="section">
         <div class="container">
             <div class="row align-items-center mainRow">
                 <div class="col-12">
-                    <h3 class="text-capitalize text-center mb-5">find us</h3>
-                    <div class="find-us-wrapper mt-4">
+                    <div class="find-us-wrapper">
                         <ul class="list-group list-group-horizontal align-items-start">
-                            @foreach ($data->find_us as $socialLink)
+                            @foreach ($data->socialLinks as $socialLink)
                                 <li class="list-group-item border-0 overflow-auto">
                                     <a href="{{ $socialLink->link }}" target="_blank" class="text-decoration-none">
-                                        <img src="{{ asset($socialLink->img) }}" alt="" class="overflow-hidden square-70">
+                                        <img src="{{ asset($socialLink->img) }}" alt="" class="overflow-hidden square-30">
+                                        {{-- <span class="social-icon">{!! $socialLink->icon !!}</span> --}}
                                         {{-- <span class="ml-2 text-capitalize">{{ $socialLink->title }}</span> --}}
                                     </a>
                                 </li>
--- resources/views/partials/index/footer.blade.php
+++ resources/views/partials/index/footer.blade.php
@@ -1,26 +1,54 @@
-<footer class="page-footer">
+<footer class="page-footer tc-black-bg">
     <div class="container-fluid footer text-center p-0">
         <div class="container w-100 py-3">
-            <div class="row">    <!-- row-eq-height"> -->
+            <div class="row">
                 <div class="col-md-6 col-sm-6 copyright-txt">
-                    <div>Copyright <i class="fas fa-copyright"></i> {{ now()->year }} |
+                    <div class="copy-right">
+                        <span class="copy-right-txt">
+                            Copyright <i class="fas fa-copyright"></i> {{ now()->year }} |
+                        </span>
                         <div class="brand d-inline-block">
-                            {{-- <img src="{{ asset('/assets/img/teacode/teacode.png') }}"
-                            class="img-fluid rounded-circle square-30" alt=""> --}}
                             <a href="/" class="d-flex align-items-center">
                                 <div class="logo position-relative">
-                                    <img src="{{ asset('/assets/img/teacode/tc-brackets.png') }}"
-                                    class="logo-brackets img-fluid rounded-circle square-30" alt="">
-                                    <img src="{{ asset('/assets/img/teacode/tc-cup.png') }}"
-                                    class="logo-cup img-fluid rounded-circle square-30 position-absolute left-0" alt="">
+                                    <img src="{{ asset('/assets/img/teacode/tc-brackets.png') }}" width="40" height="40"
+                                    class="logo-brackets img-fluid rounded-circle square-30" alt="Logo Brackets">
+                                    <img src="{{ asset('/assets/img/teacode/tc-cup.png') }}" width="40" height="40"
+                                    class="logo-cup img-fluid rounded-circle square-30 position-absolute left-0" alt="Logo Cup">
                                 </div>
                                 <span>TeaCode.ma</span>
                             </a>
                         </div>
                     </div>
+                    <div class="social-icons-wrapper">
+                        <ul class="list-group list-group-horizontal align-items-start">
+                            @foreach ($data->socialLinks->teacode as $socialLink)
+                                <li class="list-group-item border-0 overflow-auto my-0 mx-2">
+                                    <a href="{{ $socialLink->link }}" target="_blank" rel="noopener"
+                                        aria-label="{{ $socialLink->title }}"
+                                        class="text-decoration-none">
+                                        <span class="social-icon">{!! $socialLink->icon !!}</span>
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
                 <div class="col-md-6 col-sm-6 owner">
                     <p>Made with <i class="fas fa-heart"></i> by <span class="bold">Driss Boumlik</span></p>
+
+                    <div class="social-icons-wrapper">
+                        <ul class="list-group list-group-horizontal align-items-start">
+                            @foreach ($data->socialLinks->mine as $socialLink)
+                                <li class="list-group-item border-0 overflow-auto my-0 mx-2">
+                                    <a href="{{ $socialLink->link }}" target="_blank" rel="noopener"
+                                        aria-label="{{ $socialLink->title }}"
+                                        class="text-decoration-none">
+                                        <span class="social-icon">{!! $socialLink->icon !!}</span>
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
