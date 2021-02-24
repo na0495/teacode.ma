e1d68992a6e64b4cee2c83fec46aac5e11c59e33
Driss Boumlik <boumlik.t@gmail.com>
2021-02-24 17:12:58 +0100

    Fix typo

--- resources/views/partials/footer-parts/extra-links.blade.php
+++ resources/views/partials/footer-parts/extra-links.blade.php
@@ -13,7 +13,7 @@
                 <p>
                     As human being, we love bringing value to ourselves and to others.
                     Join us and level up your development skills in the process,
-                    or give 15 to 30min of your time to help others who need it, a value is added in both ways.
+                    or give 15 to 30 min of your time to help others who need it, a value is added in both ways.
                 </p>
             </div>
 
