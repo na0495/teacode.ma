5e34c307ba13789b44edfd6763591bd3f2f5eeb1
Driss Boumlik <boumlik.t@gmail.com>
2021-02-25 12:31:58 +0100

    Fix redirections for remote

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,10 @@ class HomeController extends Controller
 {
     public function home(Request $request, $var = null)
     {
+        $baseUrl = $request->getBaseUrl();
+		if (strpos($baseUrl, 'public') != false || strpos($baseUrl, 'base') != false) {
+			return \Redirect::to('https://teacode.ma');
+		}
         if ($var) {
             return redirect('/');
         }
