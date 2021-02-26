6e66fbde9f87133ea150757355e3cbdd1c2b3287
Driss Boumlik <boumlik.t@gmail.com>
2021-02-26 02:56:35 +0100

    Preload fonts for optimisation

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -21,6 +21,11 @@
         <link rel="icon" type="image/x-icon" href="{{ asset('/assets/img/teacode/teacode.ico') }}">
 
         <!-- Fonts -->
+        <link rel="preload" href="{{ asset('/webfonts/fa-solid-900.woff2') }}" as="font" type="font/woff2" crossorigin>
+        <link rel="preload" href="{{ asset('/webfonts/fa-brands-400.woff2') }}" as="font" type="font/woff2" crossorigin>
+        <link rel="preload" href="{{ asset('/assets/fonts/istokweb.woff2') }}" as="font" type="font/woff2" crossorigin>
+        <link rel="preload" href="{{ asset('/assets/fonts/UntitledSansWeb-Bold.woff2') }}" as="font" type="font/woff2" crossorigin>
+
         <link href="{{ asset('/css/externals.css') }}" rel="preload" as="style">
         <link href="{{ asset('/css/externals.css') }}" rel="stylesheet">
         <link href="{{ asset('/css/app.css') }}" rel="preload" as="style">
