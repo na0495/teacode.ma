e21d2fac5d9f7245e3f0c7b25cda38acd8f8ee9f
Driss Boumlik <boumlik.t@gmail.com>
2021-02-26 08:59:42 +0100

    Handle missing element

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,14 @@ require('./bootstrap');
 
 $(function () {
 
-    particlesJS.load('particles-js', '/plugins/particles/particles.json');
-    setTimeout(() => {
-        $('.loader-wrapper').addClass('disappear');
-    }, 500);
+    try {
+        if($('#particles-js').length) {
+            particlesJS.load('particles-js', '/plugins/particles/particles.json');
+            setTimeout(() => {
+                $('.loader-wrapper').addClass('disappear');
+            }, 500);
+        }
+    } catch (error) {
+
+    }
 });
