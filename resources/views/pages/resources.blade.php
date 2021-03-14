3e63363d2d56b344eb6084830316049a1a352145
Driss Boumlik <boumlik.t@gmail.com>
2021-03-14 08:13:15 +0100

    Add more resources links

--- resources/views/pages/resources.blade.php
+++ resources/views/pages/resources.blade.php
@@ -10,18 +10,18 @@
                 <div class="row">
                     <div class="col-lg-10 offset-lg-1 col-12">
                         @foreach ($data->resources as $resource)
-                            <div class="section">
-                                <div class="section-header">
-                                    <h4 class="text-capitalize mb-4">{{ $resource->title }}</h4>
+                            <div class="section mb-5">
+                                <div class="section-header mb-4">
+                                    <h4 class="text-capitalize">{{ $resource->title }}</h4>
                                 </div>
                                 <div class="section-body">
                                     @foreach ($resource->items as $item)
-                                        <div class="section-block">
-                                            <div class="section-block-header">
-                                                <h5 class="text-capitalize mb-3">{{ $item->title }}</h5>
+                                        <div class="section-block mb-3">
+                                            <div class="section-block-header mb-3">
+                                                <h5 class="text-capitalize">{{ $item->title }}</h5>
                                             </div>
                                             <div class="section-block-body">
-                                                <ul class="list-links">
+                                                <ul class="list-links mb-0">
                                                     @foreach ($item->links as $link)
                                                         <li class="list-link">
                                                             <div class="list-link-txt">
