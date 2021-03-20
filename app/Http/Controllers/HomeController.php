74160ef0e6fb5eddf65641cf261d19f59b2f2a89
Driss Boumlik <boumlik.t@gmail.com>
2021-03-20 15:07:37 +0100

    Move external links to properties config file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,25 +38,7 @@ class HomeController extends Controller
 
     public function gotoExternalLink(Request $request, $link)
     {
-        $links = [
-
-            'discord' => 'https://discord.gg/vKu2fkPqjY',
-
-            'workshops' => 'https://discord.gg/acVUjZ74PU',
-            'communication' => 'https://discord.gg/rSeFZZvjDY',
-            'communication-en' => 'https://discord.gg/rSeFZZvjDY',
-            'communication-fr' => 'https://discord.gg/RTycrq2Hfk',
-            'hangouts' => 'https://discord.gg/cC7kuBqJsy',
-            'pair-programming' => 'https://discord.gg/7d3mDvVFvs',
-            'mock-interview' => 'https://discord.gg/F5rBCKj2ah',
-
-            'facebook-page' => 'https://facebook.com/teacode.ma',
-            'facebook-group' => 'https://facebook.com/groups/teacode.ma',
-            'linkedin' => 'https://www.linkedin.com/company/teacodema',
-            'youtube' => 'https://youtube.com/channel/UCss61diIS1kW_TRsHMMwtwQ',
-            'twitter' => 'https://twitter.com/teacodema',
-            'instagram' => 'https://instagram.com/teacode.ma',
-        ];
+        $links = config('properties.links');
         $url = $this->home;
         if ($link && array_key_exists($link, $links)) {
             $url = $links[$link];
---
+++ config/properties.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    "links" => [
+        'discord' => 'https://discord.gg/vKu2fkPqjY',
+
+        'workshops' => 'https://discord.gg/acVUjZ74PU',
+        'communication' => 'https://discord.gg/rSeFZZvjDY',
+        'communication-en' => 'https://discord.gg/rSeFZZvjDY',
+        'communication-fr' => 'https://discord.gg/RTycrq2Hfk',
+        'hangouts' => 'https://discord.gg/cC7kuBqJsy',
+        'pair-programming' => 'https://discord.gg/7d3mDvVFvs',
+        'mock-interview' => 'https://discord.gg/F5rBCKj2ah',
+
+        'facebook' => 'https://facebook.com/teacode.ma',
+        'facebook-page' => 'https://facebook.com/teacode.ma',
+        'facebook-group' => 'https://facebook.com/groups/teacode.ma',
+        'linkedin' => 'https://www.linkedin.com/company/teacodema',
+        'youtube' => 'https://youtube.com/channel/UCss61diIS1kW_TRsHMMwtwQ',
+        'twitter' => 'https://twitter.com/teacodema',
+        'instagram' => 'https://instagram.com/teacode.ma',
+    ]
+];
