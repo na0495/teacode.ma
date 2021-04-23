8db5cb42ceb403b9466174296ae8d55d63524dc1
Driss Boumlik <idrissboumlik@gmail.com>
2021-04-23 23:53:13 +0000

    Rearrange links

--- config/properties.php
+++ config/properties.php
@@ -16,9 +16,10 @@ return [
         'facebook' => 'https://facebook.com/teacode.ma',
         'facebook-page' => 'https://facebook.com/teacode.ma',
         'facebook-group' => 'https://facebook.com/groups/teacode.ma',
-        'linkedin' => 'https://www.linkedin.com/company/teacodema',
         'youtube' => 'https://youtube.com/channel/UCss61diIS1kW_TRsHMMwtwQ',
-        'twitter' => 'https://twitter.com/teacodema',
         'instagram' => 'https://instagram.com/teacode.ma',
+        'twitter' => 'https://twitter.com/teacodema',
+        'linkedin' => 'https://www.linkedin.com/company/teacodema',
+        'patreon' => 'https://www.patreon.com/teacodema',
     ]
 ];
