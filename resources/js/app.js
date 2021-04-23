eb27e935c969645ae79e489a4fbe3fcebc772b36
Driss Boumlik <idrissboumlik@gmail.com>
2021-04-23 00:35:44 +0000

    Update dark mode

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,23 @@ require('particles.js');
 window.$ = require( 'jquery' );
 
 
+let _isActive = localStorage.getItem('isDarkModeActive');
+let _body = $(document.body);
+let _button = $('.toggle-dark-mode');
+toggleDarkMode(_button, _isActive);
+
+function toggleDarkMode(button, isActive) {
+    if (isActive) {
+        _body.addClass('dark-mode');
+        button.addClass('active');
+        localStorage.setItem('isDarkModeActive', true);
+    } else {
+        _body.removeClass('dark-mode');
+        button.removeClass('active');
+        localStorage.removeItem('isDarkModeActive');
+    }
+}
+
 $(function () {
 
     try {
@@ -13,10 +30,6 @@ $(function () {
             particlesJS.load('particles-js', '/plugins/particles/particles.min.json');
         }
 
-        let _isActive = localStorage.getItem('isDarkModeActive');
-        let _body = $(document.body);
-        let _button = $('.toggle-dark-mode');
-        toggleDarkMode(_button, _isActive);
         $(document).on('click', '.toggle-dark-mode', function () {
             let _this = $(this);
             let _isActive = !_this.hasClass('active')
@@ -28,17 +41,6 @@ $(function () {
             toggleDarkMode(_this, _isActive);
         });
 
-        function toggleDarkMode(button, isActive) {
-            if (isActive) {
-                _body.addClass('dark-mode');
-                button.addClass('active');
-                localStorage.setItem('isDarkModeActive', true);
-            } else {
-                _body.removeClass('dark-mode');
-                button.removeClass('active');
-                localStorage.removeItem('isDarkModeActive');
-            }
-        }
     } catch (error) {
 
     }
