926c0f00dcadccf23f09fd3da91b5ab408ef308d
Driss Boumlik <idrissboumlik@gmail.com>
2021-04-23 01:21:40 +0000

    Add label to button for accessibility

--- resources/views/addons/toggle-darkmode.blade.php
+++ resources/views/addons/toggle-darkmode.blade.php
@@ -1,6 +1,6 @@
 <div class="wrapper-dark-mode">
     <div class="btn-wrapper-dark-mode">
-        <button class="toggle-dark-mode">
+        <button class="toggle-dark-mode" aria-label="toggle-dark-mode">
             <i class="fas fa-moon"></i>
         </button>
     </div>
