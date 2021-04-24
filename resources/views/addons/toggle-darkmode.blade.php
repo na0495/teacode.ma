84dc109937c6734410731ba4e085cd6b5ac2eab3
Driss Boumlik <idrissboumlik@gmail.com>
2021-04-24 23:46:36 +0000

    Edit dark mode btn icon

--- resources/views/addons/toggle-darkmode.blade.php
+++ resources/views/addons/toggle-darkmode.blade.php
@@ -1,7 +1,7 @@
 <div class="wrapper-dark-mode">
     <div class="btn-wrapper-dark-mode">
         <button class="toggle-dark-mode {{ $mode . '-mode' }}" aria-label="toggle-dark-mode">
-            <i class="icon-mode fas {{ $mode . '-mode' }}"></i>
+            <i class="icon-mode {{ $mode . '-mode' }}"></i>
         </button>
     </div>
 </div>
