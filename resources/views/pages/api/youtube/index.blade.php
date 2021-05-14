38742a569d804294b02aad766e97938d732f8236
Driss Boumlik <idrissboumlik@gmail.com>
2021-05-14 03:56:39 +0000

    Rename youtube view to index

--- resources/views/pages/api/youtube/index.blade.php
+++ resources/views/pages/api/youtube/index.blade.php
@@ -4,14 +4,14 @@
 
     @include('layouts.menu')
     <div class="container-fluid p-0">
-        @include('pages.index-parts.about')
+        {{-- @include('pages.index-parts.about') --}}
         <div id="videos" class="py-5 px-4">
             <div class="container">
                 <div class="row">
                     <div class="col-12 col-lg-7">
                         <div class="video-preview">
                             <div class="watch">
-                                <iframe width="100%" height="315"
+                                <iframe
                                 src="https://www.youtube.com/embed/{{ $data->currentVideo->id }}?{{ $data->currentVideo->params }}" 
                                 title="YouTube video player" frameborder="0"
                                 allow="accelerometer; autoplay; 
@@ -19,8 +19,9 @@
                                 gyroscope; picture-in-picture" 
                                 allowfullscreen></iframe>
                             </div>
-                            <div class="description">
-                                {!! nl2br(e($data->currentVideo->description)) !!}
+                            {{-- // TODO Add chapters clicking (js) --}}
+                            <div class="video-description px-4 py-4">
+                                {!! nl2br(_($data->currentVideo->description)) !!}
                             </div>
                         </div>
                     </div>
@@ -28,43 +29,27 @@
                         <div class="video-list">
                             <ul class="list-group align-items-start">
                                 @foreach ($data->results as $result)
-                                <li class="list-group-item">
-                                    <a href="/_api/youtube/{{ $result->videoId }}">
+                                <li class="list-group-item video-item w-100 {{ $data->currentVideo->id == $result->videoId ? 'active' : '' }}">
+                                    <a href="/videos/{{ $result->videoId }}">
                                         <div class="video-data">
-                                            <div class="video-cover">
-                                                <div class="video-duration">
+                                            <div class="video-cover align-items-start">
+                                                {{-- <div class="video-duration">
                                                     <span>{{ $result->videoDuration }}</span>
-                                                </div>
-                                                <img src="{{ $result->cover->maxres->url }}" 
+                                                </div> --}}
+                                                {{-- <img src="{{ $result->cover->maxres->url }}" 
                                                 style="width: 100px; height: 56px"
                                                 width="{{ $result->cover->standard->width }}" height="{{ $result->cover->standard->height }}"
-                                                alt="">
+                                                alt=""> --}}
+                                                <div class="video-icon">
+                                                    <i class="fab fa-youtube"></i>
+                                                </div>
                                             </div>
                                             <div class="video-metadata ml-2">
                                                 <div class="video-title">
-                                                    {{ $result->videoTitle }}
+                                                    {{ \Str::limit($result->videoTitle, 50, '...') }}
                                                 </div>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                @endforeach
-                                @foreach ($data->results as $result)
-                                <li class="list-group-item">
-                                    <a href="/_api/youtube/{{ $result->videoId }}">
-                                        <div class="video-data">
-                                            <div class="video-cover">
-                                                <div class="video-duration">
-                                                    <span>{{ $result->videoDuration }}</span>
-                                                </div>
-                                                <img src="{{ $result->cover->maxres->url }}" 
-                                                style="width: 100px; height: 56px"
-                                                width="{{ $result->cover->standard->width }}" height="{{ $result->cover->standard->height }}"
-                                                alt="">
-                                            </div>
-                                            <div class="video-metadata ml-2">
-                                                <div class="video-title">
-                                                    {{ $result->videoTitle }}
+                                                <div class="video-datetime">
+                                                    {{ $result->publishedAt }}
                                                 </div>
                                             </div>
                                         </div>
