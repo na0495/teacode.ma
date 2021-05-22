ed886f8f6bb5efd1f7c5942108f7c23e0d9a473a
Driss Boumlik <idrissboumlik@gmail.com>
2021-05-22 10:31:40 +0100

    Load footer menu only on dev mode

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,9 @@
 <footer class="page-footer">
     <div class="container-fluid footer text-center">
-        @include('layouts.footer-parts.footer-menu')
-        <hr class="footer-separator">
+        @if (env('APP_ENV') != 'production')
+            @include('layouts.footer-parts.footer-menu')
+            <hr class="footer-separator">
+        @endif
         @include('layouts.footer-parts.extra-data')
         <hr class="footer-separator">
         @include('layouts.footer-parts.copy-right')
