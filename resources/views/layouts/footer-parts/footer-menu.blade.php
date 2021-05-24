101f9b6ac5a4d5b2335bf39ec89a0b268671ede8
Driss Boumlik <idrissboumlik@gmail.com>
2021-05-24 18:12:39 +0100

    Animate footer links on hover

--- resources/views/layouts/footer-parts/footer-menu.blade.php
+++ resources/views/layouts/footer-parts/footer-menu.blade.php
@@ -5,7 +5,7 @@
                 <ul class="footer-menu list-group list-group-horizontal align-items-start">
                     @foreach ($data->menuFooter as $link)
                         @if (!isset($link->hidden) || !$link->hidden)
-                            <li class="footer-menu-item list-group-item border-0 overflow-auto my-sm-0 my-2 mx-3">
+                            <li class="footer-menu-item list-group-item overflow-auto my-2 mx-3">
                                 <a href="/{{ $link->slug }}" rel="noopener" target="{{ $link->target ?? '_self' }}"
                                     aria-label="{{ $link->title }}" class="text-capitalize">
                                     {{ $link->title }}
