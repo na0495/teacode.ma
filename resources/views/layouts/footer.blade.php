a34c301eadd23d82f44039cf0e03ce347d86880e
Driss Boumlik <idrissboumlik@gmail.com>
2021-05-28 00:13:40 +0100

    Enable footer menu in production

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,7 @@
 <footer class="page-footer">
     <div class="container-fluid footer text-center">
-        @if (env('APP_ENV') != 'production')
-            @include('layouts.footer-parts.footer-menu')
-            <hr class="footer-separator">
-        @endif
+        @include('layouts.footer-parts.footer-menu')
+        <hr class="footer-separator">
         @include('layouts.footer-parts.extra-data')
         <hr class="footer-separator">
         @include('layouts.footer-parts.copy-right')
