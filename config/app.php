bfcd656b34b97c51043f3e309135cc3a5e7b806b
Driss Boumlik <idrissboumlik@gmail.com>
2021-06-18 03:15:13 +0100

    Add minify package for compiled blade views

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        HTMLMin\HTMLMin\HTMLMinServiceProvider::class
     ],
 
     /*
@@ -227,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'HTMLMin' => HTMLMin\HTMLMin\Facades\HTMLMin::class,
     ],
 
 ];
