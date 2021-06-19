6be9ee35358ac0bf82b48ab5fd2ba3e6c926b661
Driss Boumlik <idrissboumlik@gmail.com>
2021-06-19 04:56:21 +0100

    Add control for hidden links

--- resources/views/layout/footer-parts/social-links.blade.php
+++ resources/views/layout/footer-parts/social-links.blade.php
@@ -1,13 +1,15 @@
 <div class="social-icons-wrapper mt-3">
     <ul class="list-group list-group-horizontal align-items-start">
         @foreach ($data->socialLinks->teacode as $socialLink)
-            <li class="list-group-item border-0 overflow-auto my-0 mx-2">
-                <a href="/{{ $socialLink->link }}" target="_blank" rel="noopener"
-                    aria-label="{{ $socialLink->title }}"
-                    class="text-decoration-none">
-                    <span class="social-icon">{!! $socialLink->icon !!}</span>
-                </a>
-            </li>
+            @if (!isset($socialLink->hidden) || !$socialLink->hidden)
+                <li class="list-group-item border-0 overflow-auto my-0 mx-2">
+                    <a href="/{{ $socialLink->link }}" target="_blank" rel="noopener"
+                        aria-label="{{ $socialLink->title }}"
+                        class="text-decoration-none">
+                        <span class="social-icon">{!! $socialLink->icon !!}</span>
+                    </a>
+                </li>
+            @endif
         @endforeach
     </ul>
 </div>
