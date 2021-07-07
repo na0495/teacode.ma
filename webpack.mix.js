697d8fa6b05b05b11fbf3b5ae9f0071ac9b8adb2
Driss Boumlik <idrissboumlik@gmail.com>
2021-07-07 01:03:38 +0100

    Disable purgeCss for hidden classes

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,11 +16,11 @@ require('laravel-mix-purgecss');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/externals.sass', 'public/css')
     .sass('resources/sass/app.sass', 'public/css')
-    .purgeCss({
-        extend: {
-            content: [path.join(__dirname, 'database/data/**/*.json')],
-        },
-    })
+    // .purgeCss({
+    //     extend: {
+    //         content: [path.join(__dirname, 'database/data/**/*.json')],
+    //     },
+    // })
     // .postCss()
 
     .sourceMaps(true, 'source-map')
