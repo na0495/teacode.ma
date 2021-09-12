7536a9a1728c5ced45afd657d10fb1a4a7c0aabb
drissboumlik <idrissboumlik@gmail.com>
2021-09-12 01:27:11 +0100

    Fix banner diff date

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,7 +37,7 @@ class AppServiceProvider extends ServiceProvider
             }
             $view->data->banner = json_decode(\File::get(base_path() . '/database/data/banner.json'));
             $view->data->banner->formated_date = \Carbon\Carbon::createFromFormat('d-m-Y H:i', $view->data->banner->date);
-            $view->data->banner->diff = $view->data->banner->formated_date->diffInHours(now(), false);
+            $view->data->banner->diff = now()->diffInDays($view->data->banner->formated_date, false);
 
             $view->data->socialLinks = getSocialLinks();
             $view->data->footerMenu = getFooterMenu();
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
         {{-- @include('addons.google-tag-manager-body') --}}
         <div class="wrapper">
                 @if ($data->banner && !empty((array) $data->banner) && ($data->banner->diff >= 0))
-                        @include('addons.banner')
+                    @include('addons.banner')
                 @endif
                 @yield('content')
         </div>
