094df12617010b0317a908dad97601d3625dab2e
drissboumlik <idrissboumlik@gmail.com>
2021-11-15 02:17:51 +0100

    Add master page for auth

---
+++ resources/views/auth/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @include('addons.google-analytics')
+        @include('addons.hotjar')
+
+        @include('addons.brand-text')
+        @include('layout.header-parts.head-meta')
+
+        {{-- @include('addons.google-tag-manager-head') --}}
+
+        @include('layout.header-parts.assets')
+        <title>{{ $data->title ?? 'TeaCode | Turning Tea into Code' }}</title>
+    </head>
+    <body class="antialiased {{ $mode . '-mode' }}">
+        {{-- @include('addons.google-tag-manager-body') --}}
+        <div class="wrapper">
+                @yield('content')
+        </div>
+        @include('addons.toggle-darkmode')
+    </body>
+</html>
