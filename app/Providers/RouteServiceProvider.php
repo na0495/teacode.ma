2a95ed9601ad73404678623d0a7a5031babb9ae1
drissboumlik <idrissboumlik@gmail.com>
2021-11-18 15:10:15 +0100

    Fix typo

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/actions';
+    public const HOME = '/_admin/actions';
 
     /**
      * The controller namespace for the application.
