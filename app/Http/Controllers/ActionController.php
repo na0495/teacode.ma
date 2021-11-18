e0a42d35914333c14a5e34af1f5a68cd1ba3faf8
drissboumlik <idrissboumlik@gmail.com>
2021-11-18 15:10:07 +0100

    Fix get event action

--- app/Http/Controllers/ActionController.php
+++ app/Http/Controllers/ActionController.php
@@ -21,34 +21,8 @@ class ActionController extends Controller
 
     public function getEvent(Request $request, Event $event)
     {
-        if ($event->days_of_week) {
-            $e = (object) [
-                'id' => $event->id,
-                'start_time' => $event->start_time,
-                'end_time' => $event->end_time,
-                'start_date' => $event->start_date,
-                'end_date' => $event->end_date,
-                'background_color' => $event->background_color,
-                'text_color' => $event->text_color,
-                'days_of_week' => implode(',', $event->days_of_week),
-                'url' => $event->url,
-                'title' => $event->title,
-                'extended_props' => json_decode(json_encode($event->extended_props))
-            ];
-        } else {
-            $e = (object) [
-                'id' => $event->id,
-                'start' => $event->start_date . ' ' . $event->start_time,
-                'end' => $event->end_date . ' ' . $event->end_time,
-                'background_color' => $event->background_color,
-                'text_color' => $event->text_color,
-                'url' => $event->url,
-                'title' => $event->title,
-                'extended_props' => json_decode(json_encode($event->extended_props))
-            ];
-        }
         $actions = getActions();
-        return view('pages.actions.index', ['actions' => $actions, 'event' => $e]);
+        return view('pages.actions.index', ['actions' => $actions, 'event' => $event]);
     }
 
     public function addEvent(Request $request)
@@ -107,18 +81,15 @@ class ActionController extends Controller
                 $extended_props[$prop[0]] = $x;
             }
         }
-        if ($data['end_date']) {
-            $data['end_date'] = \Carbon\Carbon::createFromFormat('Y-m-d', $data['end_date']);
-        }
         if ($data['days_of_week']) {
             $data['days_of_week'] = array_map(function ($i) {
                 return (int)$i;
             }, explode(',', $data['days_of_week']));
-            $event = $event->update([
-                'start_time' =>  $data['start_time'],
-                'end_time' => $data['end_time'],
-                'start_date' => \Carbon\Carbon::createFromFormat('Y-m-d', $data['start_date']),
-                'end_date' => $data['end_date'],
+            $event->update([
+                'start_time' =>  $data['start_time'] ?? $event->start_time,
+                'end_time' => $data['end_time'] ?? $event->end_time,
+                'start_date' => $data['start_date'] ? \Carbon\Carbon::createFromFormat('Y-m-d', $data['start_date']) : $event->start_date,
+                'end_date' => $data['end_date'] ? \Carbon\Carbon::createFromFormat('Y-m-d', $data['end_date']) : $event->end_date,
                 'background_color' => $data['background_color'],
                 'text_color' => $data['text_color'],
                 'days_of_week' => $data['days_of_week'],
@@ -127,13 +98,13 @@ class ActionController extends Controller
                 'extended_props' => isset($extended_props) ? json_decode(json_encode($extended_props)) : null
             ]);
         } else {
-            $event = $event->update([
-                'start_time' =>  $data['start_time'],
-                'end_time' => $data['end_time'],
-                'start_date' => \Carbon\Carbon::createFromFormat('Y-m-d', $data['start_date']),
-                'end_date' => $data['end_date'],
-                'background_color' => $data['background_color'],
-                'text_color' => $data['text_color'],
+            $event->update([
+                'start_time' =>  $data['start_time'] ?? $event->start_time,
+                'end_time' => $data['end_time'] ?? $event->end_time,
+                'start_date' => $data['start_date'] ? \Carbon\Carbon::createFromFormat('Y-m-d', $data['start_date']) : $event->start_date,
+                'end_date' => $data['end_date'] ? \Carbon\Carbon::createFromFormat('Y-m-d', $data['end_date']) : $event->end_date,
+                'background_color' => $data['background_color'] == '#000000' ? null : $data['background_color'],
+                'text_color' => $data['text_color'] == '#000000' ? null : $data['text_color'],
                 'url' => $data['url'],
                 'title' => $data['title'],
                 'extended_props' => isset($extended_props) ? json_decode(json_encode($extended_props)) : null
