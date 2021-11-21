a13ec62b7a56f4f53f54d1d4b8b65c639484ee04
drissboumlik <idrissboumlik@gmail.com>
2021-11-21 20:12:18 +0100

    Replace next event with icon

--- resources/views/addons/banner.blade.php
+++ resources/views/addons/banner.blade.php
@@ -2,7 +2,7 @@
     <div class="banner-wrapper">
         <div class="banner-text">
             <p>
-                <span class="pre-title">Next :</span>
+                <span class="pre-title"><i class="fas fa-calendar-alt"></i></span>
                 <span class="title">
                     <a href="{{ $data->banner->url }}" rel="noopener" aria-label="Banner Title" title="{{ $data->banner->title }}"
                         target="_blank" class="link-decorated">{{ $data->banner->title }}</a>
