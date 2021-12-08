7afd375057049a583ffe9117d11217737aade538
drissboumlik <idrissboumlik@gmail.com>
2021-12-08 13:21:41 +0100

    Fix banner layout

--- resources/views/addons/banner.blade.php
+++ resources/views/addons/banner.blade.php
@@ -1,16 +1,18 @@
 <div class="banner">
-    <div class="banner-wrapper">
-        <div class="banner-text">
-            <p>
-                <span class="pre-title"><i class="fas fa-calendar-alt"></i></span>
-                <span class="title">
-                    <a href="{{ $data->banner->url }}" rel="noopener" aria-label="Banner Title" title="{{ $data->banner->title }}"
-                        target="_blank" class="link-decorated">{{ $data->banner->title }}</a>
-                </span>
-            </p>
+    <div class="container">
+        <div class="banner-wrapper">
+            <div class="banner-text">
+                <p>
+                    <span class="pre-title"><i class="fas fa-calendar-alt"></i></span>
+                    <span class="title">
+                        <a href="{{ $data->banner->url }}" rel="noopener" aria-label="Banner Title" title="{{ $data->banner->title }}"
+                            target="_blank" class="link-decorated">{{ $data->banner->title }}</a>
+                    </span>
+                </p>
+            </div>
         </div>
-    </div>
-    <div class="banner-close">
-        <i class="fas fa-times"></i>
+        <div class="banner-close">
+            <i class="fas fa-times"></i>
+        </div>
     </div>
 </div>
