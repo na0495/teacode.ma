fe9a452162d5cc89e6bc266c4fd620d281f57e03
drissboumlik <idrissboumlik@gmail.com>
2022-04-08 00:33:29 +0000

    Adapt admin area menu for mobile

--- resources/views/pages/admin/menu.blade.php
+++ resources/views/pages/admin/menu.blade.php
@@ -1,9 +1,7 @@
 <div class="menu nav-scroller py-2 {{ $mode . '-mode' }}">
     <div class="container">
-        <div class="menu-blocks d-flex justify-content-between align-items-center">
-            <div class="brand turn-trigger">
-                {{-- <img src="{{ asset('/assets/img/teacode/teacode.png') }}"
-            class="img-fluid rounded-circle square-50" alt=""> --}}
+        <div class="menu-blocks d-flex justify-content-between">
+            <div class="brand turn-trigger navbar-brand">
                 <a href="/" class="d-flex align-items-center">
                     <div class="logo logo-brand position-relative d-inline-block">
                         <img src="{{ asset('/assets/img/teacode/teacode_circle_bordred_200.png') }}" width="30"
@@ -12,27 +10,40 @@
                     <h4 class="brand-txt ms-2 d-inline-block ">TeaCode</h4>
                 </a>
             </div>
-            <nav class="nav d-flex justify-content-center">
-                @isset($menu)
-                    @foreach ($menu as $item)
-                        <a class="menu-item p-2 text-capitalize" target="_blank" href="/{{ $item->slug }}">{{ $item->title }}</a>
-                    @endforeach
-                @endisset
-                @guest
-                    @if (Route::has('login'))
-                        <a class="menu-item p-2 text-capitalize" href="{{ route('login') }}">
-                            Login
-                        </a>
-                    @endif
-                @else
-                    <a class="menu-item p-2 text-capitalize" href="{{ route('logout') }}" onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-                        Logout <i class="fas fa-power-off fs-6"></i>
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                @endguest
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container-fluid justify-content-end">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            @isset($menu)
+                                @foreach ($menu as $item)
+                                <li class="nav-item">
+                                    <a class="menu-item p-2 text-capitalize nav-link {{ request()->getRequestUri() == '/'.$item->slug ? 'active' : '' }}" href="/{{ $item->slug }}">{{ $item->title }}</a>
+                                </li>
+                                @endforeach
+                            @endisset
+                            @guest
+                                @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="menu-item p-2 text-capitalize nav-link" href="{{ route('login') }}">
+                                        Login
+                                    </a>
+                                </li>
+                                @endif
+                            @else
+                            <li class="nav-item">
+                                <a class="menu-item p-2 text-capitalize nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    Logout <i class="fas fa-power-off fs-6"></i>
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </li>
+                            @endguest
+                </div>
             </nav>
         </div>
     </div>
